f8b8f7fc1a36a897708d866b6ddc1c54f1e510e1
World Wide Cargo Network <wwcargonetwork@gmail.com>
2026-01-08 15:38:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -57,7 +57,7 @@ export function Header() {
             <NavigationMenuList>
                 {navLinks.map(link => (
                     <NavigationMenuItem key={link.label}>
-                      <Link href={link.href} legacyBehavior={false} passHref>
+                      <Link href={link.href} legacyBehavior passHref>
                           <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                           {link.label}
                           </NavigationMenuLink>
@@ -153,3 +153,4 @@ const ListItem = React.forwardRef<
   )
 })
 ListItem.displayName = "ListItem"
+
