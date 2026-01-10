974a1194a62828dd05e25b44ad5e86bc8107ee8f
World Wide Cargo Network <wwcargonetwork@gmail.com>
2026-01-10 08:38:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4478480845-31432",
+  "appId": "1:103789929072:web:9ce7ac86e03272459f8ab5",
+  "apiKey": "AIzaSyCMinGQ7Q_JBuA3AX6T8TSOATITmrfdEEI",
+  "authDomain": "studio-4478480845-31432.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "103789929072"
+};
