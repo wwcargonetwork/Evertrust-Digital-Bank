4e543080a858acaeb94dc1b3ffdaece956c8ed5d
World Wide Cargo Network <wwcargonetwork@gmail.com>
2026-01-13 10:44:39 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -57,7 +57,7 @@ export function Header() {
             <NavigationMenuList>
                 {navLinks.map(link => (
                     <NavigationMenuItem key={link.label}>
-                      <Link href={link.href} legacyBehavior passHref>
+                      <Link href={link.href} passHref>
                         <NavigationMenuLink className={navigationMenuTriggerStyle()}>
                           {link.label}
                         </NavigationMenuLink>
